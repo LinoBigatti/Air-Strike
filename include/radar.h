f22665fa6a92d31512ab6495df4a3cfef8295e80
linobigatti <linobigatti2015@gmail.com>
2019-03-08 20:50:19 -0300

    Add Graphics for the radar mode.

---
+++ include/radar.h
@@ -0,0 +1,72 @@
+
+//{{BLOCK(radar)
+
+//======================================================================
+//
+//	radar, 32x32@4, 
+//	+ 16 tiles Metatiled by 4x1 not compressed
+//	Total size: 512 = 512
+//
+//	Time-stamp: 2019-03-08, 23:44:58
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_RADAR_H
+#define GRIT_RADAR_H
+
+#define radarTilesLen 512
+extern const unsigned int radarTiles[128];
+
+#endif // GRIT_RADAR_H
+
+//}}BLOCK(radar)
+
+//{{BLOCK(radar_window)
+
+//======================================================================
+//
+//	radar_window, 32x32@4, 
+//	+ 16 tiles Metatiled by 4x1 not compressed
+//	Total size: 512 = 512
+//
+//	Time-stamp: 2019-03-08, 23:44:58
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_RADAR_WINDOW_H
+#define GRIT_RADAR_WINDOW_H
+
+#define radar_windowTilesLen 512
+extern const unsigned int radar_windowTiles[128];
+
+#endif // GRIT_RADAR_WINDOW_H
+
+//}}BLOCK(radar_window)
+
+//{{BLOCK(Shared)
+
+//======================================================================
+//
+//	radar, 16x16@4, 
+//	+ palette 6 entries, not compressed
+//	Total size: 12 = 12
+//
+//	Time-stamp: 2019-03-08, 23:44:58
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_RADARPAL_H
+#define GRIT_RADARPAL_H
+
+#define radarPalLen 12
+extern const unsigned short radarPal[6];
+
+#endif // GRIT_RADARPAL_H
+
+//}}BLOCK(Shared)
